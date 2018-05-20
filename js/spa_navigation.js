80642919f0b534632896eec7b7ab23cc4dc6859a
Alexander Buhlayou <a_buglaev@gmx.com>
2018-05-20 10:24:03 +0300

    nav comment added

--- js/spa_navigation.js
+++ js/spa_navigation.js
@@ -1,36 +1,35 @@
-
+//Не то чтобы это норм, когда стили в навигации. Но, мб позже сюда еще что-то добавляться будет, тогда стили перенесу
 if (!location.hash) location.hash = '#thread1';
 
 let hashchangeHandler = function(ev){
     switch (location.hash) {
         case '#thread1':
-        setFirstThread();
-        break;
+            setFirstThread();
+            break;
         case '#thread2':
-        setSecondThread();
-        break;
+            setSecondThread();
+            break;
         case '#thread3':
-        setThirdThread();
-        break;
-
+            setThirdThread();
+            break;
         default:
-        setFirstThread();
-        break;
+            setFirstThread();
+            break;
     }
 }
-//Стили в скрипте навигации? А что...
+
 let setFirstThread = function() {
     document.getElementsByTagName('body')[0].style.background = 'linear-gradient(to bottom right, wheat, oldlace, wheat, oldlace, wheat, oldlace)';
     document.getElementById('root').style.background = 'oldlace';
 }
 
 let setSecondThread = function() {
-    document.getElementsByTagName('body')[0].style.background = 'linear-gradient(to bottom right, #ccf, #aaf, #ccf, #aaf, #ccf, #aaf)';
+    document.getElementsByTagName('body')[0].style.background = 'linear-gradient(to bottom right, #ddf, #aaf, #ddf, #aaf, #ddf, #aaf)';
     document.getElementById('root').style.background = '#ddf';
 }
 
 let setThirdThread = function() {
-    document.getElementsByTagName('body')[0].style.background = 'linear-gradient(to bottom right, #cfc, #afa, #cfc, #afa, #cfc, #afa)';
+    document.getElementsByTagName('body')[0].style.background = 'linear-gradient(to bottom right, #dfd, #afa, #dfd, #afa, #dfd, #afa)';
     document.getElementById('root').style.background = '#dfd';
 }
 
