efa9bd5add47c02e371f61bbe05b2de7efbcd391
Александр Буглаёв <a_buglaev@gmx.com>
2022-01-05 19:56:47 +0300

    Update API.js

--- js/API.js
+++ js/API.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-let serverEndpoint = '3.121.57.183';
+let serverEndpoint = '3.64.28.37';
   let sendPost = function(_formData){
     console.log('trying to send...');
 	  for (var value of _formData.values()) {
